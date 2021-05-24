b92cf67bf2736eb087a6076c09d821d84fb5869a
Umar Haroon <umar.m.haroon@gmail.com>
2021-05-24 12:56:22 -0600

    add redirects

--- src/App.js
+++ src/App.js
@@ -3,21 +3,24 @@ import './App.css';
 import MainScreen from './MainScreen';
 import PrivacyScreen from './PrivacyScreen';
 import TermsScreen from './TermsScreen';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Router>
-          <Route path="/Privacy" component={PrivacyScreen}>
+        <Switch>
+          <Route path="/Privacy">
             <PrivacyScreen />
           </Route>
-          <Route path="/Terms" component={TermsScreen}>
+          <Route path="/Terms">
             <TermsScreen />
           </Route>
           <Route>
             <MainScreen />
           </Route>
+        </Switch>
+
       </Router>
     </div>
   );
